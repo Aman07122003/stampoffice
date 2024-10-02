7eb95a2260e98a7a1696f35d02c6813debc990ae
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2024-10-02 17:44:32 +0530

    updates

--- app/page.js
+++ app/page.js
@@ -4,16 +4,16 @@ import Image from 'next/image';
 
 const Page = () => {
   return (
-    <div className='h-[90vh] w-full flex justify-center items-center bg-gradient-to-b from-[#edac66] via-[#ff9900] to-[#6b3503]'>
-      <div className='h-2/3 w-1/2 flex flex-col justify-center items-center text-5xl font-bold text-start text-[#6b3503]'>
-        <p className='text-center'>
+    <div className='h-[90vh] w-full flex md:flex-row flex-col justify-center items-center bg-gradient-to-b from-[#edac66] via-[#ff9900] to-[#6b3503]'>
+      <div className='h-1/3 md:w-1/2 order-2 mt-16 flex flex-col justify-center items-end md:items-start text-5xl font-bold text-start text-[#6b3503]'>
+        <div className='md:text-start text-center w-full'>
            Unite with Stamp <br /> Enthusiasts Across India! <br /> Discover, Collect, and Share <br /> Philatelic Treasures.
-        </p>
-        <div className='mt-6'>
-           <button className='shadow-xl h-12 text-xl w-60  rounded-xl bg-red-600 text-yellow-200 flex items-center justify-center '>Join the Community</button>
+        </div>
+        <div className='mt-6 w-full md:w-1/2 flex justify-center items-center'>
+           <button className='shadow-xl h-12 text-xl w-60  rounded-xl bg-red-700 text-yellow-300 flex items-center justify-center '>Join the Community</button>
         </div>
       </div>
-      <div className='h-2/3 w-1/2 flex justify-center items-center'>
+      <div className='h-1/3 w-96 md:w-1/2 order-1 flex justify-center items-center'>
         <Image src={Hero} alt="Hero Image" className='object-contain' />
       </div>
     </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
- "use client"
+"use client";
 
 import React, { useState } from 'react';
 import Image from 'next/image';
@@ -18,7 +18,7 @@ const Navbar = () => {
     <div className='h-24 w-full flex justify-between items-center font-bold bg-[#edac66] text-lg p-2'>
       <div className='flex gap-2 ml-3 w-1/5'>
         <Image src={Logo} alt="StampNation Logo" className='h-20 w-24' />
-        <h1 className='items-center flex text-2xl'>StampNation</h1>
+        <h1 className='items-center flex text-2xl text-amber-950 font-extrabold font-mono'>StampNation</h1>
       </div>
 
       {/* Hamburger Icon for Mobile */}
@@ -27,12 +27,12 @@ const Navbar = () => {
       </div>
 
       {/* Menu Items */}
-      <div className={`flex justify-between items-center w-3/6 mr-5 font-bold text-xl ${isOpen ? 'flex-col' : 'hidden'} md:flex md:flex-row`}>
+      <div className={`flex justify-between items-center w-3/6 mr-5 font-bold hidden md:block text-xl ${isOpen ? 'hidden' : 'flex-col'} md:flex md:flex-row`}>
         <ul className='cursor-pointer'>Home</ul>
         <ul className='cursor-pointer'>Product</ul>
         <ul className='cursor-pointer'>News</ul>
         <ul className='cursor-pointer'>Account</ul>
-        <button className='h-12 text-xl w-32 rounded-xl  bg-red-600 text-yellow-200 flex items-center justify-center shadow-xl'>
+        <button className='h-12 text-xl w-32 rounded-xl bg-red-600 text-yellow-200 flex items-center justify-center shadow-xl'>
           Premium
         </button>
       </div>
@@ -40,12 +40,13 @@ const Navbar = () => {
       {/* Mobile Menu Toggle */}
       {isOpen && (
         <div className='absolute top-24 left-0 w-full bg-[#edac66] flex flex-col items-center md:hidden'>
-          <ul className='p-4 cursor-pointer'>Home</ul>
-          <ul className='p-4 cursor-pointer'>Product</ul>
-          <ul className='p-4 cursor-pointer'>News</ul>
-          <button className='h-12 text-xl w-32 rounded-xl  bg-red-600 text-yellow-200 flex items-center justify-center mb-4'>
+          <ul className='p-4 cursor-pointer w-full text-center border-b border-gray-300'>Home</ul>
+          <ul className='p-4 cursor-pointer w-full text-center border-b border-gray-300'>Product</ul>
+          <ul className='p-4 cursor-pointer w-full text-center border-b border-gray-300'>News</ul>
+          <ul className='p-4 cursor-pointer w-full text-center border-b border-gray-300'>Account</ul>
+          <div className='p-4 cursor-pointer w-full text-center justify-center items-center border-b border-gray-300'><button className='h-12 text-xl w-32 rounded-xl bg-red-700 text-yellow-300'>
             Premium
-          </button>
+          </button></div>
         </div>
       )}
     </div>
